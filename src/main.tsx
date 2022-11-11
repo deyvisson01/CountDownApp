9905d44376914879f495d3c16cfe0c7fb55bbe72
Deyvisson Souza <deyvisson_souza01@hotmail.com>
2022-11-11 19:05:28 -0300

    feat: Adding theme provider and global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
 import RouterApp from "./Routes";
 import {store} from './store/store'
+import theme from "./styles/theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterApp />
+      <ThemeProvider theme={theme}>
+        <RouterApp />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
